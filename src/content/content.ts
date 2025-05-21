0d52be7866fe3911e84ec3cb4bc02e4928c295b7
Alireza Eshghi <es.alrza@gmail.com>
2025-05-21 20:00:46 +0330

    Add logic to content script to send update ranks for storage on page refresh

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,13 +1,25 @@
 import "./content.scss";
 import { StorageService } from "../shared/services/storage-service";
 import { SerpHelper } from "../shared/helpers/serp-helper";
-
+import { RankStorageService } from "../shared/services/rank-storage-service"
+import { SearchHelper } from "../shared/helpers/search-helper";
+const rankStorageService = new RankStorageService();
 const storageService = new StorageService();
 const serpHelper = new SerpHelper();
 
 // An observer to observe DOM changes and work on infinite scroll style search results display
 // There's some space for optimization because it's been called many times during DOM creation
+let lastUrl: string | null = null;
 const observer = new MutationObserver(async (mutationsList, observer) => {
+    let googleUrl = new URL(window.location.href);
+    const keyword = SearchHelper.getKeywordFromUrl(googleUrl);
+    const currentUrl = googleUrl.href;
+    if (lastUrl != currentUrl) {
+        lastUrl = currentUrl;
+        let resultItems = await serpHelper.getFullResaultItems(googleUrl);
+        let rankStorageList = await rankStorageService.findRanks(resultItems, keyword);
+        rankStorageService.addRank(rankStorageList);
+    }
     const sites = await storageService.getSites();
     if (sites) {
         const resultItems = serpHelper.getResultItems(document);
@@ -54,3 +66,4 @@ const getPlacement = (itemIndexInPage: number): number => {
     const skip = Number(loc.get('start')) ?? 0;
     return skip + itemIndexInPage;
 }
+
