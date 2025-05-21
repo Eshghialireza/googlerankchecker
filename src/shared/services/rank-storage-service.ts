690f19f5e4fa9a6f11c7b14b1a86240bcafc03b3
Alireza Eshghi <es.alrza@gmail.com>
2025-05-21 19:55:26 +0330

    Add findRanks method to rank-storage-service to extract rank for storage

--- src/shared/services/rank-storage-service.ts
+++ src/shared/services/rank-storage-service.ts
@@ -1,6 +1,11 @@
 import { AddRankResponseModel } from "../models/add-rank-response";
 import { RankStorageModel } from "../models/rank-storage";
+import { SiteStorageModel } from "../models/site-storage";
+import { SerpHelper } from "../helpers/serp-helper";
+import { CommonHelper } from "../helpers/common-helper";
+
 export class RankStorageService {
+    private serpHelper = new SerpHelper();
     constructor() { };
     public addRank(rankStorageList: RankStorageModel[]): Promise<AddRankResponseModel[]> {
         return new Promise((resolve, reject) => {
@@ -24,22 +29,22 @@ export class RankStorageService {
 
                         if (targetWithKeyword) {
                             var todayRank = targetWithKeyword.rankHistory.find(rankHistory => rankHistory.date === today);
-                            
+
                             if (!todayRank) {
-                              targetWithKeyword.rankHistory.push({ date: today, rank: newRank.keyWords[0].rankHistory[0].rank });
-                                addedResult.push({added:true,item:{hostname:newRank.hostname,keyWords:[targetWithKeyword]}});
+                                targetWithKeyword.rankHistory.push({ date: today, rank: newRank.keyWords[0].rankHistory[0].rank });
+                                addedResult.push({ added: true, item: { hostname: newRank.hostname, keyWords: [targetWithKeyword] } });
                             } else {
-                                addedResult.push({ added: false, item: {hostname:newRank.hostname,keyWords:[targetWithKeyword]}});
+                                addedResult.push({ added: false, item: { hostname: newRank.hostname, keyWords: [targetWithKeyword] } });
                             }
                         } else {
                             targetSite.keyWords.push({
                                 keyword: newRank.keyWords[0].keyword,
                                 rankHistory: [...newRank.keyWords[0].rankHistory]
                             });
-                            addedResult.push({added:true,item:{hostname:newRank.hostname,keyWords:[{keyword:newRank.keyWords[0].keyword,rankHistory:newRank.keyWords[0].rankHistory}]}});
+                            addedResult.push({ added: true, item: { hostname: newRank.hostname, keyWords: [{ keyword: newRank.keyWords[0].keyword, rankHistory: newRank.keyWords[0].rankHistory }] } });
                         }
                     }
-                });                
+                });
                 chrome.storage.sync.set({ 'myranks': updatedRanks }, () => {
                     resolve(addedResult);
                 });
@@ -48,14 +53,38 @@ export class RankStorageService {
     }
     public removeRanks(name: string): void {
         chrome.storage.sync.get('myranks', (data) => {
-        const mySites: RankStorageModel[] = data.myranks || [];
+            const mySites: RankStorageModel[] = data.myranks || [];
 
-             var targetSite=mySites.find(site=>site.hostname===name);
+            var targetSite = mySites.find(site => site.hostname === name);
             if (targetSite) {
                 var index = data.myranks.indexOf(targetSite);
                 data.myranks.splice(index, 1);
             }
             chrome.storage.sync.set({ 'myranks': data.myranks });
+        })
+    }
+    public findRanks(resultItems: Element[], keyword: any): Promise<RankStorageModel[]> {
+        return new Promise((resolve, reject) => {
+            var rankStorageList: RankStorageModel[] = [];
+            chrome.storage.sync.get('mysites', (data) => {
+                if (data.mysites && data.mysites.length > 0) {
+                    const mysites: SiteStorageModel[] = data.mysites;
+                    const today = new Date().toISOString().split("T")[0];
+                    // Throw an error if the keyword is null or undefined
+                    if (!keyword) throw new Error('Keyword not found');
+                    for (let i = 0; i < resultItems.length; i++) {
+                        let url = this.serpHelper.getLinkFromResultItem(resultItems[i]);
+                        let domain = CommonHelper.getDomainNameFromUrl(url);
+                        const targetSite = mysites.find(site => site.hostname.toLowerCase() === domain.toLowerCase())
+                        if (targetSite) {
+                            if (!rankStorageList.find(item => item.hostname.toLowerCase() === targetSite.hostname.toLowerCase())) {
+                                rankStorageList.push({ hostname: domain, keyWords: [{ keyword: keyword, rankHistory: [{ date: today, rank: i + 1 }] }] })
+                            }
+                        }
+                    }
+                }
+            })
+            resolve(rankStorageList);
         })
     }
 }
