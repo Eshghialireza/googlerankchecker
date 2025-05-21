80526107ed253212df77f5dcbdc71edb10246e0a
Alireza Eshghi <es.alrza@gmail.com>
2025-05-21 19:53:04 +0330

    Add getFullResaultItems method to serp-helper to fetch top 100 google results

--- src/shared/helpers/serp-helper.ts
+++ src/shared/helpers/serp-helper.ts
@@ -1,6 +1,7 @@
 // SERP(Search Engine Result Page) helper
+import { HttpHelper } from "../helpers/http-helper"
 export class SerpHelper {
-
+    private httpHelper = new HttpHelper();
     readonly Result_Item_Selector: string = 'div[jscontroller="SC7lYd"]';
 
     getResultItems(resultsContainer: any) {
@@ -8,9 +9,20 @@ export class SerpHelper {
 
         return resultItems;
     }
+    async getFullResaultItems(googleurl: URL) {
+        if (googleurl.href.includes("google.") && googleurl.href.includes("/search")) {
+            googleurl.searchParams.set('num', '100');
+            googleurl.searchParams.set('start', '0');
+            const res = await this.httpHelper.httpGetAsync(googleurl.href);
+            let element = document.createElement('html');
+            element.innerHTML = res;
+            let resultItems = this.getResultItems(element);
+            return resultItems;
+        }
+    }
     getLinkFromResultItem(resutlItem: any): string {
         let cite = resutlItem.getElementsByTagName('a')[0];
-        console.log('cite:',cite)
+        console.log('cite:', cite)
         console.log('url:', cite.href)
         return cite.href;
     }
